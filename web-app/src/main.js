fb9f37408e5576d153f012c97ed9d8fff3b9e634
Iacopo <iacopo.ghila@gmail.com>
2022-02-13 17:21:32 +0100

    first import

--- web-app/src/main.js
+++ web-app/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+
+app.config.globalProperties.myKey = '49b11a04577b4b818c848eef087c4315';
+
+app.use(router)
+.use(VueAxios, axios)
+.mount('#app')
